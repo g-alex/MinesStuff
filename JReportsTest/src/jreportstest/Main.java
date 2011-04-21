eb270cef9a18bc8584c8bc9ca0ecbe296ba78abb
g-alex <alexandre.garnier.86@gmail.com>
2011-04-21 17:19:28 +0200

    JDBC Connection in JReports

--- JReportsTest/src/jreportstest/Main.java
+++ JReportsTest/src/jreportstest/Main.java
@@ -4,6 +4,7 @@
  */
 package jreportstest;
 
+import com.mysql.jdbc.Driver;
 import groovy.lang.Tuple;
 import java.io.File;
 import java.io.FileInputStream;
@@ -12,12 +13,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.JasperRunManager;
 import net.sf.jasperreports.engine.data.JRCsvDataSource;
@@ -36,10 +43,21 @@ public class Main {
     private static Map<String, Class> FIELDS = new HashMap<String, Class>();
 
     public static void main(String[] args)
-            throws JRException, FileNotFoundException, IOException {
+            throws JRException, FileNotFoundException, IOException, SQLException {
 
-        parseData();
+        Driver monDriver = new Driver();
+        DriverManager.registerDriver(monDriver);
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jasper_database", "root", "tototiti");
 
+        JasperDesign jspDesign = JRXmlLoader.load("src/reports/JDBCreport.jrxml");
+        JasperReport jspReport = JasperCompileManager.compileReport(jspDesign);
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jspReport, null, connection);
+
+        JasperExportManager.exportReportToPdfFile(jasperPrint, "src/reports/JDBCreport.pdf");
+
+//                parseData();
+//
 //        JasperDesign jspDesign = JRXmlLoader.load("src/reports/reportTest.jrxml");
 //        JasperReport jspReport = JasperCompileManager.compileReport(jspDesign);
 //
@@ -58,48 +76,48 @@ public class Main {
 //        fos.close();
     }
 
-    private static void parseData() throws FileNotFoundException, IOException {
-        InputStreamReader isr = new InputStreamReader(new FileInputStream("src/reports/reportTest.csv"));
-        LineNumberReader lnr = new LineNumberReader(isr);
-        String line;
-        List<Long> times = new ArrayList<Long>();
-        List<Double> consumptions = new ArrayList<Double>();
-        while ((line = lnr.readLine()) != null) {
-            if (lnr.getLineNumber() > 1) {
-                String[] fields = line.split(",");
-                times.add(Long.decode(fields[0]));
-                consumptions.add(Double.valueOf(fields[1]).doubleValue());
-            } // if
-        } // while
-        String überString = "min,max,moy\n" + min(consumptions) + ","
-                + max(consumptions) + "," + moy(consumptions);
-        System.out.println(überString);
-        System.out.println(max(consumptions));
-        System.out.println(min(consumptions));
-        System.out.println(moy(consumptions));
-    } // void parseData()
-
-    private static Double max(List<Double> values) {
-        Double max = null;
-        for (Double val : values) {
-            max = (max == null || val > max) ? val : max;
-        } // for
-        return max;
-    }
-
-    private static Double min(List<Double> values) {
-        Double min = null;
-        for (Double val : values) {
-            min = (min == null || val < min) ? val : min;
-        } // for
-        return min;
-    }
-
-    private static Double moy(List<Double> values) {
-        double moy = 0;
-        for (double val : values) {
-            moy += val;
-        } // for
-        return new Double((double) (int) (10 * moy / values.size()) / 10);
-    }
+//    private static void parseData() throws FileNotFoundException, IOException {
+//        InputStreamReader isr = new InputStreamReader(new FileInputStream("src/reports/reportTest.csv"));
+//        LineNumberReader lnr = new LineNumberReader(isr);
+//        String line;
+//        List<Long> times = new ArrayList<Long>();
+//        List<Double> consumptions = new ArrayList<Double>();
+//        while ((line = lnr.readLine()) != null) {
+//            if (lnr.getLineNumber() > 1) {
+//                String[] fields = line.split(",");
+//                times.add(Long.decode(fields[0]));
+//                consumptions.add(Double.valueOf(fields[1]).doubleValue());
+//            } // if
+//        } // while
+//        String überString = "min,max,moy\n" + min(consumptions) + ","
+//                + max(consumptions) + "," + moy(consumptions);
+//        System.out.println(überString);
+//        System.out.println(max(consumptions));
+//        System.out.println(min(consumptions));
+//        System.out.println(moy(consumptions));
+//    } // void parseData()
+//
+//    private static Double max(List<Double> values) {
+//        Double max = null;
+//        for (Double val : values) {
+//            max = (max == null || val > max) ? val : max;
+//        } // for
+//        return max;
+//    }
+//
+//    private static Double min(List<Double> values) {
+//        Double min = null;
+//        for (Double val : values) {
+//            min = (min == null || val < min) ? val : min;
+//        } // for
+//        return min;
+//    }
+//
+//    private static Double moy(List<Double> values) {
+//        double moy = 0;
+//        for (double val : values) {
+//            moy += val;
+//        } // for
+//        return new Double((double) (int) (10 * moy / values.size()) / 10);
+//    }
 }
