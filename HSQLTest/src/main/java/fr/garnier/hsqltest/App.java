091113208bd54cd56501752390ea3e0675d677ff
g-alex <alexandre.garnier.86@gmail.com>
2011-05-09 09:57:51 +0200

    Simple HSQLTest added

---
+++ HSQLTest/src/main/java/fr/garnier/hsqltest/App.java
@@ -0,0 +1,13 @@
+package fr.garnier.hsqltest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
